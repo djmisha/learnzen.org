1ed1e289e088927f53cf681c1ad1adcb206bee5c
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2020-01-12 05:52:19 -0800

    intro scene

--- src/appinfo.js
+++ src/appinfo.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import IconInfo from './images/icon-info.js'
 
-
 class Appinformation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      infoClass: 'the-info-icon info-normal',
-      overlayClass: 'info-overlay info-hidden',
+      infoClass: 'the-info-icon info-change',
+      overlayClass: 'info-overlay info-visible',
     };
     this.handleinfoChange = this.handleinfoChange.bind(this);
   }
   handleinfoChange() {
-    const changeinfo = this.state.infoClass === 'the-info-icon info-normal' ? 'the-info-icon info-change' : 'the-info-icon info-normal';
-    const changeOverlay = this.state.overlayClass === 'info-overlay info-hidden' ? 'info-overlay info-visible' : 'info-overlay info-hidden';
+    const changeinfo = this.state.infoClass === 'the-info-icon info-change' ? 'the-info-icon info-normal' : 'the-info-icon info-change';
+    const changeOverlay = this.state.overlayClass === 'info-overlay info-visible' ? 'info-overlay info-hidden' : 'info-overlay info-visible';
     this.setState({
       infoClass: changeinfo,
       overlayClass: changeOverlay,
@@ -26,7 +25,11 @@ class Appinformation extends React.Component {
         <div className={this.state.overlayClass}>
         <span>Tao Te Ching</span>
         <span>道德经</span>
-        <p>The Tao Te Ching is a fundamental text for both philosophical and religious Taoism and is traditionally credited to the 6th-century BC sage Laozi. It also strongly influenced other schools of Chinese philosophy and religion, including Legalism, Confucianism, and Buddhism, which was largely interpreted through the use of Taoist words and concepts when it was originally introduced to China.</p>
+        <p>The Tao Te Ching is traditionally credited to the 6th-century BC sage Laozi. It is a fundamental text for both philosophical and religious Taoism. 
+        <br />
+        <br />
+        Explore the teaching of the Tao verses by using the navigation below, uou can read all verses, view one at at time or read a random verse cliking the buttons.  Ready to learn?</p>
+        <p>Click here to start...</p>
         </div>
       </div>
     )
