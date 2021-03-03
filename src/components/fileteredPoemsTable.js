8b967d0c7ee6cf7e0f1ea337ac3c07935d3ca259
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2021-03-03 13:03:20 -0800

    filtered table

--- src/components/fileteredPoemsTable.js
+++ src/components/fileteredPoemsTable.js
@@ -59,7 +59,7 @@ class FileteredPoemsTable extends React.Component {
             visiblePoem = visiblePoem + 1;
         }
 
-        // Update Zen Count
+        // Update Zen Count in Local Storage
         let updatedPoemCount = Number(localStorage.zenCount) + 0.01;
         updatedPoemCount = updatedPoemCount.toFixed(2);
         localStorage.setItem("zenCount", updatedPoemCount);
